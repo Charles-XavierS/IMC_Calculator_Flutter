cad77338cc34d831e8bd73c84eda29d2354bdcd3
Charles Xavier <charlesxsaldanha@hotmail.com>
2023-09-26 19:40:57 -0300

    feat: Criado model para sqlite

---
+++ lib/models/imc_sqlite_model.dart
@@ -0,0 +1,19 @@
+class ImcSqliteModel {
+  int _id = 0;
+
+  String _imc = '';
+
+  ImcSqliteModel(this._imc);
+
+  int get id => _id;
+
+  set id(int id) {
+    _id = id;
+  }
+
+  String get imc => _imc;
+
+  set imc(String imc) {
+    _imc = imc;
+  }
+}
